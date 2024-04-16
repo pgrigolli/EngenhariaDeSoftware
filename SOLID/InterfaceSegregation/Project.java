ea156f62f5e6333854b4b054ab606ba634b9935c
pgrigolli <pedrogrigolli30@gmail.com>
2024-04-16 11:43:30 -0300

    Comments added

--- SOLID/InterfaceSegregation/Project.java
+++ SOLID/InterfaceSegregation/Project.java
@@ -7,14 +7,11 @@ public class Project {
 
 interface TaxService{
 
-    public double brazilTaxService(double amount);
-    public double usaTaxService(double amount);
-    public double mexicanTaxService(double amount);
+    public double taxService(double amount);
+    public double brazilTaxService(double amount); // Only for brazilian people
+    public double usaTaxService(double amount); // Only for american people
+    public double mexicanTaxService(double amount); // Only for mexican people
 
 
 }
 
-class brasilTaxService{
-
-    
-}
