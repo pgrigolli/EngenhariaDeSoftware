863f3ed17a6c2b47f6ca8c479bbc4735e650adec
pgrigolli <pedrogrigolli30@gmail.com>
2024-04-16 11:33:15 -0300

    InterfaceSegregationCommit

---
+++ SOLID/InterfaceSegregation/Project.java
@@ -0,0 +1,16 @@
+package InterfaceSegregation;
+
+public class Project {
+    
+}
+
+
+interface TaxService{
+
+    public double BrazilTaxService(double amount);
+    public double USATaxService(double amount);
+    public double MexicanTaxService(double amount);
+
+
+}
+
