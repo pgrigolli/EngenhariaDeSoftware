71a11928063e943d4ba9173339732f92250ecff1
Pedro Grigolli <pedrogrigolli30@gmail.com>
2024-05-09 15:19:11 -0300

    Structural Pattern

---
+++ DesignPatterns/EstructuralPattern/Project.java
@@ -0,0 +1,44 @@
+// Adapter Design Pattern
+
+interface FahrenheitSensor {
+    double getTemperature();
+}
+
+class CelsiusTemperatureSensor {
+    double getTemperatureInCelsius() {
+        return 25.0; // Exemplo
+    }
+}
+
+class TemperatureAdapter implements FahrenheitSensor {
+    private CelsiusTemperatureSensor celsiusSensor;
+
+    TemperatureAdapter(CelsiusTemperatureSensor celsiusSensor) {
+        this.celsiusSensor = celsiusSensor;
+    }
+
+    @Override
+    public double getTemperature() {
+
+        double celsiusTemp = celsiusSensor.getTemperatureInCelsius();
+
+        double fahrenheitTemp = (celsiusTemp * 9/5) + 32; // Conversão de Celsius para Fahrenheit
+
+        return fahrenheitTemp;
+
+    }
+}
+
+public class Project {
+    public static void main(String[] args) {
+
+        CelsiusTemperatureSensor celsiusSensor = new CelsiusTemperatureSensor();
+
+        FahrenheitSensor adapter = new TemperatureAdapter(celsiusSensor);
+
+        double temperatureInFahrenheit = adapter.getTemperature();
+
+        System.out.println("Temperature in Fahrenheit: " + temperatureInFahrenheit);
+
+    }
+}
